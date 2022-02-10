fdbb1f02c55491546ffbf1d3687fb186255c994d
nuran gok <goknurann@gmail.com>
2022-02-10 14:08:23 +0100

    son classlar

---
+++ src/test/java/tests/day22/C01_TestNGReports.java
@@ -0,0 +1,34 @@
+package tests.day22;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
+
+import java.io.IOException;
+
+public class C01_TestNGReports extends TestBaseRapor {
+
+    @Test
+    public void windowHandlereusableTest() throws IOException {
+        extentTest=extentReports.createTest("window handle","title ile 2.sayfaya gecis testi");
+        Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
+        extentTest.info("herokuapp sayfasina gidildi");
+        Driver.getDriver().findElement(By.linkText("Click Here")).click();
+        extentTest.info("yeni window linkine tilandi");
+        ReusableMethods.switchToWindow("New Window");
+        extentTest.info("reusable method kullanarak acilan iknci sayfaya gecildi");
+        String expectedTitle="New Window";
+        String actualTitle=Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
+        extentTest.pass("title n beklenen deger ile ayni oldugu test edildi");
+        ReusableMethods.getScreenshot("WindowSwitch");
+        Driver.closeDriver();
+    }
+
+
+
+
+}
